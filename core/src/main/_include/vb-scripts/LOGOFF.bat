b0d8e55f31030add38622fc363bfb85b410ab795
Simon Meggle <simon.meggle@consol.de>
2014-05-09 11:33:28 +0200

    used SAKULI_HOME in batchfile

--- core/src/main/_include/vb-scripts/LOGOFF.bat
+++ core/src/main/_include/vb-scripts/LOGOFF.bat
@@ -1,11 +1,12 @@
 REM LOGOFF.bat
 REM Wrapper Script for logoff.vbs
 REM Simon Meggle <simon.meggle@consol.de>, 2014
-REM Place this file on the Desktop. 
-REM Execute with admin privileges to safely disconnect the RDP session
+REM Set a user environent variable %SAKULI_HOME% to the akuli root folder. 
+REM Then place this file on the Desktop. 
+REM Execute with admin privileges to safely disconnect the current RDP session
 REM and attach it to the local console. See logoff.vbs for more details. 
 
 echo off
-cscript.exe /NoLogo C:\sakuli\logoff.vbs
+cscript.exe /NoLogo %SAKULI_HOME%\_include\vb-scripts\logoff.vbs
 
 
