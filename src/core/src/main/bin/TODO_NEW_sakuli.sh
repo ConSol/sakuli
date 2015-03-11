82a1b6850ef8391ce9e71bdb13a108023fbac85d
toschneck <tobias.schneck@consol.de>
2015-03-11 12:33:01 +0100

    #60 move some new files to the new structure // modify TODO_NEW_sakuli.sh to the expected version

--- src/core/src/main/bin/TODO_NEW_sakuli.sh
+++ src/core/src/main/bin/TODO_NEW_sakuli.sh
@@ -2,7 +2,7 @@
 
 SUITE=/home/tschneck/git-files/sakuli/src/core/_dev_test_suites/_testsuite1_Ubuntu_BB
 
-MAIN_FOLDER=$SAKULI_HOME
 SAKULI_JARS=$SAKULI_HOME/libs/java
 
-java -classpath $SAKULI_JARS/sakuli.jar:$SAKULI_JARS/* org.sakuli.starter.SakuliStarter -run "$SUITE" "$MAIN_FOLDER" "$MAIN_FOLDER/../sahi"
+#sakuli.sh -run ../testsuite -sahi_home ../../sahi -sakuli_home ../SAKULI_APP/sakuli/sakuli-v0.4.8-new-structure-SNAPSHOT
+java -classpath $SAKULI_JARS/sakuli.jar:$SAKULI_JARS/* org.sakuli.starter.SakuliStarter -run "$SUITE" -sakuli_home "$SAKULI_HOME" -sahi_home "$MAIN_FOLDER/../sahi"
