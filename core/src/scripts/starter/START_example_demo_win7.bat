a8df3d853f4c710a7e20358b61b82ae30c088a5d
Simon Meggle <simon.meggle@consol.de>
2015-10-06 16:05:05 +0200

    Fixed path in starter script

--- core/src/scripts/starter/START_example_demo_win7.bat
+++ core/src/scripts/starter/START_example_demo_win7.bat
@@ -8,7 +8,7 @@ set TEST_SUITE_FOLDER=%SAKULI_HOME%\sakuli_test_suites\%SUITE%
 
 set SAKULI_JARS=%SAKULI_HOME%\bin\lib\*;%SAKULI_HOME%\bin\lib\resource;
 
-cscript.exe %SAKULI_HOME%\scripts\helper\vb_scripts\killproc.vbs -f %SAKULI_HOME%\scripts\helper\vb_scripts\procs_to_kill.txt
+cscript.exe %SAKULI_HOME%\scripts\helper\killproc.vbs -f %SAKULI_HOME%\scripts\helper\procs_to_kill.txt
 
 echo jar-file: %SAKULI_JARS%
 java -Dsikuli.Home=%SAKULI_HOME%\bin\lib -classpath %SAKULI_HOME%\bin\sakuli.jar;%SAKULI_JARS% org.sakuli.starter.SakuliStarter -run "%TEST_SUITE_FOLDER%" "%SAKULI_HOME%\_include"
