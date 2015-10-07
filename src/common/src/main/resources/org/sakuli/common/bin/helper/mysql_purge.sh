ea447a111b3fd7fd2435fc325d0f3836c98ea692
Simon Meggle <simon.meggle@consol.de>
2015-10-07 16:28:59 +0200

    Adapted mysql_purge.sh to new table names, added parameter. #10

--- src/common/src/main/resources/org/sakuli/common/bin/helper/mysql_purge.sh
+++ src/common/src/main/resources/org/sakuli/common/bin/helper/mysql_purge.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # Automatic cleanup for Sakuli result tables
+# add to $OMD_ROOT/etc/cron.d/sakuli: 
+# 02 12 * * * $OMD_ROOT/local/bin/mysql_purge.sh 90 > /dev/null 2>&1
+
+if [ $# -ne 1 ]
+then
+        echo "Wrong number of argumentsi. Arg 1 must be the number of days db data should be kept."
+        exit 1
+fi
+
+DAYS=$1
 
 mysql sakuli<<EOFMYSQL
-DELETE FROM sakuli_suites where time < DATE_SUB(NOW(), INTERVAL 3 DAY);
-DELETE FROM sakuli_cases where time < DATE_SUB(NOW(), INTERVAL 3 DAY);
-DELETE FROM sakuli_steps where time < DATE_SUB(NOW(), INTERVAL 3 DAY);
+DELETE FROM sakuli_suites where time < DATE_SUB(NOW(), INTERVAL $DAYS  DAY);
+DELETE FROM sakuli_cases where time < DATE_SUB(NOW(), INTERVAL $DAYS DAY);
+DELETE FROM sakuli_steps where time < DATE_SUB(NOW(), INTERVAL $DAYS DAY);
 EOFMYSQL
 
 echo "FLUSH QUERY CACHE;" | mysql
@@ -20,4 +30,3 @@ do
         echo "done."
     done
 done
-
