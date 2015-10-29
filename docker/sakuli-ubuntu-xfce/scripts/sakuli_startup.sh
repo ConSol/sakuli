26ded6add69cc1f725045164544fdc6ed8a6721e
toschneck <tobias.schneck@consol.de>
2015-10-29 10:56:00 +0100

    * adjust sakuli_startup.sh script to make container reattachble * change Dockerfile to entrypoint to start sakuli automatically on startup and also be able to pass commands like  `bin/bash` to `docker run` or special arguments for sakuli like for example `docker run consol/sakuli-centos-xfce '--run $SAKULI_TEST_SUITE --browser chrome'`

--- docker/sakuli-ubuntu-xfce/scripts/sakuli_startup.sh
+++ docker/sakuli-ubuntu-xfce/scripts/sakuli_startup.sh
@@ -7,7 +7,30 @@
 find $SAKULI_TEST_SUITE -type d | while read dir ; do chmod 777 "$dir" ; done
 find $SAKULI_TEST_SUITE -type f | while read file ; do chmod 666 "$file" ; done
 
-$SAKULI_HOME/bin/sakuli.sh --run $SAKULI_TEST_SUITE
+for i in "$@"
+do
+
+echo -e "\n----------- resolve script arguments -------------"
+echo -e "\narg_orig: $i"
+i=$(eval echo $i)
+echo "arg_resolved: $i"
+
+case $i in
+    # if option starts with `-` the script arguments are interpreted as arguments for Sakuli
+    -*)
+    echo -e "\n------------------ START SAKULI ------------------"
+    $SAKULI_HOME/bin/sakuli.sh $i
+    ;;
+
+    *)
+    # unknown option ==> call command
+    echo -e "\n----------------- EXECUTE COMMAND ----------------"
+    echo -e "\ncmd: $i"
+    exec $i
+    ;;
+esac
+done
+
 res=$?
 echo "SAKULI_RETURN_VAL: $res"
 exit $res
