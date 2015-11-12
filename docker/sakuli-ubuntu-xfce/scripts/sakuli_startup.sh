9f9809d53aef745b404e5b3732319da75f697fd9
Simon Meggle <simon.meggle@consol.de>
2015-11-12 16:50:42 +0100

    sakuli_startup.sh: changed chmod commands for setting file/dir permissions

--- docker/sakuli-ubuntu-xfce/scripts/sakuli_startup.sh
+++ docker/sakuli-ubuntu-xfce/scripts/sakuli_startup.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-# call `vnc_startup.sh` from the root container
 /root/scripts/vnc_startup.sh
 
 # modify $SAKULI_TEST_SUITE permissions to ensure, that volume-mounted log files can be deleted afterwards
-find $SAKULI_TEST_SUITE -type d | while read dir ; do chmod 777 "$dir" ; done
-find $SAKULI_TEST_SUITE -type f | while read file ; do chmod 666 "$file" ; done
+chmod -r a+w $SAKULI_TEST_SUITE
 
 for i in "$@"
 do
+    echo -e "\n----------- resolve script arguments -------------"
+    echo -e "\narg_orig: $i"
+    i=$(eval echo $i)
+    echo "arg_resolved: $i"
 
-echo -e "\n----------- resolve script arguments -------------"
-echo -e "\narg_orig: $i"
-i=$(eval echo $i)
-echo "arg_resolved: $i"
+    case $i in
+        # if option starts with `-` the script arguments are interpreted as arguments for Sakuli
+        -*)
+        echo -e "\n------------------ START SAKULI ------------------"
+        $SAKULI_HOME/bin/sakuli.sh $i
+        ;;
 
-case $i in
-    # if option starts with `-` the script arguments are interpreted as arguments for Sakuli
-    -*)
-    echo -e "\n------------------ START SAKULI ------------------"
-    $SAKULI_HOME/bin/sakuli.sh $i
-    ;;
-
-    *)
-    # unknown option ==> call command
-    echo -e "\n----------------- EXECUTE COMMAND ----------------"
-    echo -e "\ncmd: $i"
-    exec $i
-    ;;
-esac
+        *)
+        # unknown option ==> call command
+        echo -e "\n----------------- EXECUTE COMMAND ----------------"
+        echo -e "\ncmd: $i"
+        exec $i
+        ;;
+    esac
 done
 
 res=$?
