a9934d7a848fbe479c3bdae07004a12e84ae38a6
tschneck <tobias.schneck@consol.de>
2015-11-18 19:47:26 +0100

    #24 fix `start_example_windows.bat` script

--- example_test_suites/start_example_windows.bat
+++ example_test_suites/start_example_windows.bat
@@ -1,11 +1,9 @@
-
-if "%SAKULI_HOME%" == "" (
-  SET SAKULI_HOME=..\
+if "%SAKULI_HOME%"=="" (
    echo "Environment variable 'SAKULI_HOME' have not been set!" \
           "Please ensure that 'SAKULI_HOME' points to the installation folder of your expected Sakuli version!"
    goto end
 )
 
 SET basedir=%~dp0
-%SAKULI_HOME%/bin/sakuli.bat --run "%basedir%/example_windows"
+%SAKULI_HOME%/bin/sakuli.bat --run %basedir%\example_windows
 :end
