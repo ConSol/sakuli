cf06004c461823dea9eb6a5abb687e66ad1e7d61
toschneck <tobias.schneck@consol.de>
2015-11-18 19:05:59 +0100

    #24 add some example starter scripts

---
+++ example_test_suites/start_example_xfce.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$SAKULI_HOME" ]; then
+    echo "Environment variable 'SAKULI_HOME' have not been set!" \
+        "Please ensure that 'SAKULI_HOME' points to the installation folder of your expected Sakuli version!" \
+        && exit 6
+fi
+
+basedir=`dirname $0`
+$SAKULI_HOME/bin/sakuli.sh --run "$basedir/example_xfce"
