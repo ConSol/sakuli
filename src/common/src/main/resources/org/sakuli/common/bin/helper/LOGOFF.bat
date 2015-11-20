f372e33af5a3e9b6e1489a8ad44a6b910a1eccdb
Simon Meggle <simon.meggle@consol.de>
2015-11-20 08:49:50 +0100

    LOGOFF.bat: comment

--- src/common/src/main/resources/org/sakuli/common/bin/helper/LOGOFF.bat
+++ src/common/src/main/resources/org/sakuli/common/bin/helper/LOGOFF.bat
@@ -1,5 +1,8 @@
 REM LOGOFF.bat
 REM Wrapper Script for logoff.vbs
+REM Run this script with admin privileges from a Windows Desktop to automatically 
+REM disconnect the current RDP session and redirect it to the local console.
+REM SAKULI_HOME must be set as a system variable.
 REM Simon Meggle <simon.meggle@consol.de>, 2014
 REM See https://github.com/ConSol/sakuli/blob/master/docs/additional-settings.md
 echo off
