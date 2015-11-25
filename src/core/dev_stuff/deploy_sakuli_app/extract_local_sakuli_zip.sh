8728c143680106665f7b4a64c7af100cf5de694e
toschneck <tobias.schneck@consol.de>
2015-11-25 14:03:50 +0100

    update dev-deploy_sakuli_app scripts

--- src/core/dev_stuff/deploy_sakuli_app/extract_local_sakuli_zip.sh
+++ src/core/dev_stuff/deploy_sakuli_app/extract_local_sakuli_zip.sh
@@ -5,7 +5,7 @@ set -x
 SAKULI_PROJECT_HOME=~/git-files/sakuli
 SAKULI_INST_FOLDER=$SAKULI_PROJECT_HOME/sakuli_app
 SAKULI_TARGET_DIR=$SAKULI_PROJECT_HOME/src/core/target/install
-SAKULI_VERSION=0.9.1-SNAPSHOT
+SAKULI_VERSION=0.9.2-SNAPSHOT
 
 rm -rf $SAKULI_INST_FOLDER
 
@@ -16,7 +16,3 @@ echo "current dir $(dirname $0)"
 cp $(dirname $0)/start_*.sh $SAKULI_INST_FOLDER
 cp $(dirname $0)/run_*.sh $SAKULI_INST_FOLDER
 chmod -v +x $(find  /home/tschneck/git-files/sakuli/sakuli_app/ -name "*.sh")
-
-#/home/tschneck/git-files/sakuli/sakuli_app/sakuli/sakuli-v0.9.1-SNAPSHOT/bin/sakuli.sh
-#/home/tschneck/git-files/sakuli/sakuli_app/sakuli/sakuli-v0.9.1-SNAPSHOT/bin/sakuli.sh
-#/home/tschneck/git-files/sakuli/sakuli_app/sakuli/sakuli-v0.9.1-SNAPSHOT/bin/sakuli.sh
--- src/core/dev_stuff/deploy_sakuli_app/start_example_ubuntu.sh
+++ src/core/dev_stuff/deploy_sakuli_app/start_example_ubuntu.sh
@@ -2,12 +2,10 @@
 
 SAKULI_PROJECT_HOME=~/git-files/sakuli
 SAKULI_INST_FOLDER=$SAKULI_PROJECT_HOME/sakuli_app
-SAKULI_VERSION=0.9.1-SNAPSHOT
+SAKULI_VERSION=0.9.2-SNAPSHOT
 export SAKULI_HOME=$SAKULI_INST_FOLDER/sakuli/sakuli-v$SAKULI_VERSION
-SAHI_HOME=$SAKULI_PROJECT_HOME/sahi
 
 SUITE=example_test_suites/example_ubuntu
-SUITE=src/core/dev_stuff/suites/ubuntu
 
 VNC_DISPLAY=1
 
@@ -18,6 +16,6 @@ export DISPLAY=:$VNC_DISPLAY
 #vncserver should run!
 #vncserver $DISPLAY -depth 24 -geometry 1024x768 &
 
-$SAKULI_INST_FOLDER/sakuli/sakuli-v$SAKULI_VERSION/bin/sakuli.sh --run "$SAKULI_PROJECT_HOME/$SUITE" --sahi_home "$SAHI_HOME"
+$SAKULI_INST_FOLDER/sakuli/sakuli-v$SAKULI_VERSION/bin/sakuli.sh --run "$SAKULI_PROJECT_HOME/$SUITE"
 
 #killall Xvnc4
