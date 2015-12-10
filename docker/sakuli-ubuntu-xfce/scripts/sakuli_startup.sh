698de7c9a32280f6a70f04e3a402bb5651502b23
toschneck <tobias.schneck@consol.de>
2015-12-10 23:06:32 +0100

    fix docker startup script

--- docker/sakuli-ubuntu-xfce/scripts/sakuli_startup.sh
+++ docker/sakuli-ubuntu-xfce/scripts/sakuli_startup.sh
@@ -9,7 +9,8 @@ chmod -R a+rw $SAKULI_TEST_SUITE
 # If not, assume that CMD was not meant as an argument
 # for sakuli.sh (=ENTRYPOINT). Hence, try to execute CMD standalone.
 if [ "${1:0:1}" == "-" ]; then
-        $SAKULI_HOME/bin/sakuli.sh $*
+        i=$(eval echo $*)
+        $SAKULI_HOME/bin/sakuli.sh $i
 else
         exec $1
 fi
