37cb994b7c2809a3d3a1875fb18eeef13d0a9830
toschneck <tobias.schneck@consol.de>
2016-01-31 22:29:12 +0100

    update structurizer

--- src/docs-structurizer/src/main/java/org/sakuli/docs/StructurizerDiagrammBuilder.java
+++ src/docs-structurizer/src/main/java/org/sakuli/docs/StructurizerDiagrammBuilder.java
@@ -44,10 +44,11 @@ public class StructurizerDiagrammBuilder {
         sakuliSystem.setLocation(Location.Internal);
         SoftwareSystem systemUnderTest = model.addSoftwareSystem("SUT", "System under Test");
         systemUnderTest.setLocation(Location.External);
+        systemUnderTest.addTags("SUT");
         SoftwareSystem monitioringSystem = model.addSoftwareSystem("Nagios Monitoring", "A nagios based monitoring system");
-        systemUnderTest.setLocation(Location.External);
+        monitioringSystem.setLocation(Location.External);
         SoftwareSystem database = model.addSoftwareSystem("MySQL Database", "A MySQL database to store the Sakuli results");
-        systemUnderTest.setLocation(Location.External);
+        database.setLocation(Location.External);
         Person tester = model.addPerson("Tester", "A software tester");
         Person operator = model.addPerson("Operator", "A software operator");
 
@@ -60,43 +61,6 @@ public class StructurizerDiagrammBuilder {
         sakuliSystem.delivers(tester, "Testing Results");
         monitioringSystem.delivers(operator, "Monitoring Repors");
 
-
-        //containers
-        Container starter = sakuliSystem.addContainer("JavaScript Starter", "Starter for JavaScript Testcases",
-                ".sh / .bat script and Java binaries");
-        Container javastarter = sakuliSystem.addContainer("Java Starter", "Starter for Java Testcases",
-                "TestNG Test");
-        tester.uses(starter, "uses");
-        tester.uses(javastarter, "uses");
-
-        ComponentFinder componentFinderStarter = new ComponentFinder(
-                starter, "org.sakuli.starter",
-                new SpringComponentFinderStrategy(),
-                new NameSuffixComponentFinderStrategy("Starter")
-        );
-        componentFinderStarter.findComponents();
-        starter.getComponents().stream()
-                .filter(c -> c.getTechnology().equals(SpringComponentFinderStrategy.SPRING_COMPONENT))
-//                .filter(c -> c.getTechnology().contains("JDBC"))
-                .forEach(c -> {
-//                    c.uses(sakuliSystem, "starts");
-                    c.addTags("Spring Component");
-                });
-        starter.uses(sakuliSystem, "contains");
-
-        ComponentFinder componentFinderJavaStarter = new ComponentFinder(
-                javastarter, "org.sakuli.javaDSL",
-                new SpringComponentFinderStrategy(),
-                new NamePrefixComponentFinderStrategy("AbstractSakuliTest"),
-                new NameSuffixComponentFinderStrategy("Service")
-        );
-        componentFinderJavaStarter.findComponents();
-        javastarter.getComponents().stream()
-                .filter(c -> c.getTechnology().equals(SpringComponentFinderStrategy.SPRING_COMPONENT))
-                .forEach(c -> c.addTags("Spring Component"));
-        javastarter.uses(sakuliSystem, "contains");
-
-
         ViewSet viewSet = workspace.getViews();
         SystemContextView contextView = viewSet.createContextView(sakuliSystem);
         contextView.setKey("context");
@@ -105,35 +69,102 @@ public class StructurizerDiagrammBuilder {
         contextView.addAllElements();
 
 
+        //javascripte starter
+        Container jsStarter = sakuliSystem.addContainer("JavaScript Starter", "Starter for JavaScript Testcases",
+                ".sh / .bat script and Java binaries");
+
+        tester.uses(jsStarter, "uses");
+
+        new ComponentFinder(
+                jsStarter, "org.sakuli.starter",
+                new SpringComponentFinderStrategy(),
+                new NameSuffixComponentFinderStrategy("Starter")
+        ).findComponents();
+        jsStarter.getComponents().stream()
+                .filter(c -> c.getTechnology().equals(SpringComponentFinderStrategy.SPRING_COMPONENT))
+                .forEach(c -> c.addTags("Spring Component"));
+        jsStarter.uses(sakuliSystem, "contains");
+        jsStarter.uses(jsStarter.getComponentWithName("SakuliStarter"), "calls");
+        ComponentView jsStarterView = viewSet.createComponentView(jsStarter);
+        jsStarterView.setKey("starter-components");
+        jsStarterView.add(tester);
+        jsStarterView.add(jsStarter);
+        jsStarterView.addAllComponents();
+
+        //java Starter
+        Container javastarter = sakuliSystem.addContainer("Java Starter", "Starter for Java Testcases",
+                "TestNG Test");
+        tester.uses(javastarter, "uses");
+        new ComponentFinder(
+                javastarter, "org.sakuli.javaDSL",
+                new SpringComponentFinderStrategy(),
+                new NamePrefixComponentFinderStrategy("AbstractSakuliTest"),
+                new NameSuffixComponentFinderStrategy("Service")
+        ).findComponents();
+        javastarter.getComponents().stream()
+                .filter(c -> c.getTechnology().equals(SpringComponentFinderStrategy.SPRING_COMPONENT))
+                .forEach(c -> c.addTags("Spring Component"));
+        javastarter.uses(sakuliSystem, "contains");
+        javastarter.uses(javastarter.getComponentWithName("InitializingService"), "calls");
+        ComponentView javaStarterView = viewSet.createComponentView(javastarter);
+        javaStarterView.add(javastarter);
+        javaStarterView.add(tester);
+        javaStarterView.addAllComponents();
+
+        //actions
+        Container actions = sakuliSystem.addContainer("Sakuli Actions", "Execution the HTML/UI Actions", "Java Library");
+        actions.uses(systemUnderTest, "controls");
+        Component sahi = actions.addComponent("Sahi Library", "Sahi HTML control functions");
+        sahi.uses(systemUnderTest, "controls");
+        sahi.addTags("LIB");
+        Component sikuli = actions.addComponent("SikuliX Library", "SikuliX UI control functions");
+        sikuli.uses(systemUnderTest, "controls");
+        sikuli.addTags("LIB");
+        ComponentFinder componentFinder = new ComponentFinder(
+                actions, "org.sakuli.actions",
+                new SpringComponentFinderStrategy()
+                , new NamePrefixComponentFinderStrategy("Appli")
+                , new NamePrefixComponentFinderStrategy("Region")
+                , new NamePrefixComponentFinderStrategy("Environment")
+                , new NameSuffixComponentFinderStrategy("Action")
+        );
+        componentFinder.findComponents();
+        componentFinder.enrichComponent("Component", "Class", null, null, null);
+        actions.getComponentWithName("RegionImpl").uses(sikuli, "calls");
+        actions.getComponentWithName("Environment").uses(sikuli, "calls");
+        //actions.getComponentWithName("Application").uses(sikuli, "calls");
+
+        actions.getComponentWithName("Action").uses(sahi, "calls");
+        actions.getComponentWithName("Action").uses(actions.getComponentWithName("TestCaseAction"), "");
+        actions.uses(actions.getComponentWithName("Action"), "calls");
+        actions.getComponents().stream()
+                .filter(c -> c.getTechnology().equals(SpringComponentFinderStrategy.SPRING_COMPONENT))
+                .forEach(c -> c.addTags("Spring Component"));
+        ComponentView actionView = viewSet.createComponentView(actions);
+        actionView.add(actions);
+        actionView.add(systemUnderTest);
+        actionView.addAllComponents();
+
+        //containers view
         ContainerView containerView = viewSet.createContainerView(sakuliSystem);
         containerView.setKey("containers");
-        //containerView.addAllPeople();
+        containerView.addAllPeople();
         containerView.addAllSoftwareSystems();
         containerView.addAllContainers();
 
-        ComponentView starterView = viewSet.createComponentView(starter);
-        starterView.setKey("starter-components");
-        //componentView.addAllPeople();
-        starterView.add(starter);
-        starterView.addAllComponents();
-
-
-        ComponentView javaStarterView = viewSet.createComponentView(javastarter);
-        javaStarterView.add(javastarter);
-        javaStarterView.addAllComponents();
-
         // link the architecture model with the code
-        starter.getComponents().stream().forEach(c -> System.out.println(c.getSourcePath()));
-        starter.getComponents().stream().forEach(c -> c.setSourcePath("https://github.com/ConSol/sakuli/tree/master/src/core"));
-
+        jsStarter.getComponents().stream().forEach(c -> c.setSourcePath("https://github.com/ConSol/sakuli/tree/master/src/core"));
+        actions.getComponents().stream().forEach(c -> c.setSourcePath("https://github.com/ConSol/sakuli/tree/master/src/core"));
         javastarter.getComponents().stream().forEach(c -> c.setSourcePath("https://github.com/ConSol/sakuli/tree/master/src/java-dsl"));
 
 
         Styles styles = viewSet.getConfiguration().getStyles();
         styles.addElementStyle(Tags.PERSON).setShape(Shape.Person);
-        styles.addElementStyle(Tags.RELATIONSHIP).color("#147FA3");
+        styles.addElementStyle(Tags.PERSON).background("#FF9673");
         styles.addElementStyle("Spring Component").background("#91D366");
+        styles.addElementStyle("SUT").background("#FAFA55");
         styles.addElementStyle(Tags.ELEMENT).color("#147FA3");
+        styles.addElementStyle("LIB").color("#96781B");
 
         StructurizrClient structurizrClient = new StructurizrClient("d6d9e2c4-c866-4a51-b70c-064b43c4e045", "e2dd9013-9515-4b94-8732-c2fa0d4e9282");
         structurizrClient.mergeWorkspace(8341, workspace);
