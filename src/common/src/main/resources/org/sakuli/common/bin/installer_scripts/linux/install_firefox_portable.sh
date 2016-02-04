619ae3720d88b86725a25d0737038bb820bac570
toschneck <tobias.schneck@consol.de>
2016-02-04 11:38:25 +0100

    #24 add firefox portable to linux installer // fix #157 execution of not fitting shell script

---
+++ src/common/src/main/resources/org/sakuli/common/bin/installer_scripts/linux/install_firefox_portable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! "${1:0:1}" == "" ]; then
+    FF_VERS=$1
+    if [ ! "${2:0:1}" == "" ]; then
+        FF_INST=$2
+        echo "download Firefox $FF_VERS and install it to '$FF_INST'."
+        mkdir -p "$FF_INST"
+        FF_URL=http://releases.mozilla.org/pub/firefox/releases/$FF_VERS/linux-x86_64/en-US/firefox-$FF_VERS.tar.bz2
+        cd "$FF_INST" && wget wget -qO- $FF_URL | tar xvj
+        exit $?
+    fi
+fi
+echo "Script parameter are not set correctly please call it like 'install_firefox_portable.sh [version] [install path]'"
+exit -1
+
