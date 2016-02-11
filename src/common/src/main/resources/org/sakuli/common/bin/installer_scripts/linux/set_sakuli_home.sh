98615519baff079ea6d5d22861b133c554cd9e1c
toschneck <tobias.schneck@consol.de>
2016-02-11 16:37:47 +0100

    close #158 add firefox variables for UI testing to linux installer

--- src/common/src/main/resources/org/sakuli/common/bin/installer_scripts/linux/set_sakuli_home.sh
+++ src/common/src/main/resources/org/sakuli/common/bin/installer_scripts/linux/set_sakuli_home.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 function remove_sakuli_home {
-    echo "remove 'SAKULI_HOME' from 'bash.rc'"
-    unset SAKULI_HOME
+    echo "remove 'SAKULI_HOME' from '.bashrc'"
     [ -r ~/.bashrc ] && sed -i '/export SAKULI_HOME/d' ~/.bashrc
 }
 
 function set_sakuli_home {
     if [ -d "$1" ]; then
-        echo "set 'SAKULI_HOME' in 'bash.rc' to: $1"
-        echo "export SAKULI_HOME=$1" >> ~/.bashrc
-        export SAKULI_HOME=$1
+        echo "set 'SAKULI_HOME' in '.bashrc' to: $1"
+        echo "export SAKULI_HOME=\"$1\"" >> ~/.bashrc
     else
         echo "new 'SAKULI_HOME' path '$1' does not exists!"
         exit 1
@@ -24,7 +22,6 @@ function set_path {
     else
         echo "add 'SAKULI_HOME/bin' to PATH"
         echo -e "export PATH=\$SAKULI_HOME/bin:\$PATH" >> ~/.bashrc
-        export PATH=$SAKULI_HOME/bin:$PATH
     fi
 }
 
@@ -32,8 +29,8 @@ function remove_path {
     if grep -q SAKULI_HOME/bin: ~/.bashrc 2>/dev/null; then
         echo "remove 'SAKULI_HOME' from PATH"
         [ -r ~/.bashrc ] && sed -i '/SAKULI_HOME\/bin/d' ~/.bashrc
-        PATH=${PATH/":$SAKULI_HOME"/} # delete any instances in the middle or at the end
-        PATH=${PATH/"$SAKULI_HOME:"/} # delete any instances at the beginning
+        #PATH=${PATH/":$SAKULI_HOME"/} # delete any instances in the middle or at the end
+        #PATH=${PATH/"$SAKULI_HOME:"/} # delete any instances at the beginning
     else
       echo "no update of 'PATH' needed!"
     fi
@@ -43,6 +40,6 @@ remove_sakuli_home
 remove_path
 if [ ! "${1:0:1}" == "" ]; then
     sakuli_inst_path=$(eval echo $*)
-    set_sakuli_home $sakuli_inst_path
+    set_sakuli_home "$sakuli_inst_path"
     set_path
 fi
---
+++ src/common/src/main/resources/org/sakuli/common/bin/installer_scripts/linux/set_ui_testing_variables.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function remove_var {
+    echo "remove '$1' from 'bash.rc'"
+    unset $1
+    regex="/export $1/d"
+#    echo "regex to remove: $regex"
+    [ -r ~/.bashrc ] && sed -i "$regex" ~/.bashrc
+}
+
+function set_var {
+    echo "set '$1' in 'bash.rc' to: 1"
+    echo "export $1='1'" >> ~/.bashrc
+    export $1='1'
+}
+
+FF_VARS=("MOZ_OOP_DISABLE_PLUGINS" "MOZ_DISABLE_AUTO_SAFE_MODE")
+
+for ((i=0; i < ${#FF_VARS[@]}; i++))
+do
+  cur_var=${FF_VARS[$i]}
+  echo $cur_var
+  remove_var $cur_var
+  if [ "${1:0:3}" == "set" ]; then
+    set_var $cur_var
+  fi
+done
+
