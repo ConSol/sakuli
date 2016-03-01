e18477d7d5b916b0a16c2d356c796139e7b4d841
toschneck <tobias.schneck@consol.de>
2016-03-01 10:43:49 +0100

    some updates

--- src/core/dev_stuff/suites/ubuntu/labs/tc_labs.js
+++ src/core/dev_stuff/suites/ubuntu/labs/tc_labs.js
@@ -18,10 +18,11 @@
 
 _dynamicInclude($includeFolder);
 var testCase = new TestCase(40, 50);
+var env = new Environment();
 
 //vars consol.labs
 var $cl_home = "http://labs.consol.de";
-var $cl_projekte = "Projekte";
+var $cl_projekte = "Projects";
 var $cl_c_mysql_h = "check_mysql_health";
 var $cl_c_oracle_h = "check_oracle_health";
 
