bcfaa0176cd9bc7cb944b5333be7a56615a7c760
Tobias Schneck <tobias.schneck@consol.de>
2017-07-03 20:39:51 +0200

    #247 fix sakuli-java installation script for docker

--- docker/sakuli-client/src_java/common/install/sakuli.sh
+++ docker/sakuli-client/src_java/common/install/sakuli.sh
@@ -20,7 +20,8 @@ for javasuite in "java-selenium-example" "java-example";  do
     ## clean target after build, so only dependencies are here
     mvn test clean -Duser.home=$HOME -Dtest=TriggerDownload -DfailIfNoTests=false -Dsakuli.version=$SAKULI_VERSION
 done
-mv $tmpdir/sakuli-examples-feature-sakuli-se/java-example/ "$SAKULI_TEST_SUITE"
+rm -rf "$SAKULI_TEST_SUITE"
+mv $tmpdir/sakuli-examples-feature-sakuli-se/java-selenium-example "$SAKULI_TEST_SUITE"
 ls -la "$SAKULI_TEST_SUITE"
 rm -rvf $tmpdir
 
