38b78f236b11264fec32bd80462d08d6e14592e0
Tobias Schneck <tobias.schneck@consol.de>
2017-07-03 16:28:20 +0200

    #247 add sakuli-se-example to docker java build

--- docker/sakuli-client/src_java/common/install/sakuli.sh
+++ docker/sakuli-client/src_java/common/install/sakuli.sh
@@ -5,19 +5,24 @@ set -e
 EXAMPLE_URL=https://github.com/ConSol/sakuli-examples
 echo "Install Sakuli Java Example:  Maven  $EXAMPLE_URL to $SAKULI_TEST_SUITE"
 
-mkdir -p /tmp/sakuli-example
-cd /tmp/sakuli-example
-wget $EXAMPLE_URL/archive/master.zip
+tmpdir=/tmp/sakuli-example
+mkdir -p $tmpdir
+cd $tmpdir
+#TODO TS change to master
+# https://github.com/ConSol/sakuli-examples/archive/feature/sakuli-se.zip
+wget $EXAMPLE_URL/archive/feature/sakuli-se.zip
 unzip *.zip
 mkdir -p "$SAKULI_TEST_SUITE"
-mv sakuli-examples-master/java-example/* "$SAKULI_TEST_SUITE"
-rm -rf /tmp/sakuli-example
-
-echo "Download Sakuli dependencies for v$SAKULI_VERSION"
-cd $SAKULI_TEST_SUITE
-ls -la
-## clean target after build, so only dependencies are here
-mvn test clean -Duser.home=$HOME -Dtest=TriggerDownload -DfailIfNoTests=false -Dsakuli.version=$SAKULI_VERSION
+for javasuite in "java-selenium-example" "java-example";  do
+    echo "Download Sakuli dependencies for v$SAKULI_VERSION"
+    cd $tmpdir/sakuli-examples-feature-sakuli-se/$javasuite
+    ls -la
+    ## clean target after build, so only dependencies are here
+    mvn test clean -Duser.home=$HOME -Dtest=TriggerDownload -DfailIfNoTests=false -Dsakuli.version=$SAKULI_VERSION
+done
+mv $tmpdir/sakuli-examples-feature-sakuli-se/java-example/ "$SAKULI_TEST_SUITE"
+ls -la "$SAKULI_TEST_SUITE"
+rm -rvf $tmpdir
 
 echo "add  -Dsakuli.version=$SAKULI_VERSION to Maven startup"
 echo "export MAVEN_OPTS=-Dsakuli.version=$SAKULI_VERSION" >> $HOME/.bashrc
