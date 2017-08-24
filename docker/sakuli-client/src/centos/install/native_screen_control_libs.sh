c6e0e8b7c5a8bb293f0b13e13fd8310f9f438dfc
Tobias Schneck <tobias.schneck@consol.de>
2017-08-24 18:57:47 +0200

    try to fix centos docker build (missing package libxkbcommon)

--- docker/sakuli-client/src/centos/install/native_screen_control_libs.sh
+++ docker/sakuli-client/src/centos/install/native_screen_control_libs.sh
@@ -12,6 +12,12 @@ yum -y install leptonica tesseract tesseract-langpack-deu
 
 # some missing ui controll tools
 # wmctrl  (checked by: https://www.virustotal.com/de/url/394da2ac7e9bbbf5d10a3f304cc913459fc38f66019a4c3ba0fd4afd8f48e64f/analysis/1465466765/)
-yum -y install xdotool redhat-lsb-core psmisc https://labs.consol.de/sakuli/install/3rd-party/rpm/wmctrl-1.07-17.gf.el7.x86_64.rpm
+# libxkbcommon (checked by https://www.virustotal.com/de/file/5490412624f96da2d86c37c047394eb62ac261aa69a7883372fe9d5071302ff7/analysis/1503593211/, orig: ftp://195.220.108.108/linux/fedora/linux/releases/24/Everything/x86_64/os/Packages/l/libxkbcommon-0.5.0-4.fc24.x86_64.rpm
+yum -y install \
+    https://labs.consol.de/sakuli/install/3rd-party/rpm/libxkbcommon-0.5.0-4.fc24.x86_64.rpm \
+    xdotool \
+    redhat-lsb-core \
+    psmisc \
+    https://labs.consol.de/sakuli/install/3rd-party/rpm/wmctrl-1.07-17.gf.el7.x86_64.rpm
 
 yum clean all
