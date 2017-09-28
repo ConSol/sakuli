e6bd7a476346a05c7c56d393f97b0122b82afee3
Tobias Schneck <tobias.schneck@consol.de>
2017-09-28 20:53:40 +0200

    #247 change example installation of java examples backt to master

--- docker/sakuli-client/src_java/common/install/sakuli.sh
+++ docker/sakuli-client/src_java/common/install/sakuli.sh
@@ -8,20 +8,18 @@ echo "Install Sakuli Java Example:  Maven  $EXAMPLE_URL to $SAKULI_TEST_SUITE"
 tmpdir=/tmp/sakuli-example
 mkdir -p $tmpdir
 cd $tmpdir
-#TODO TS change to master
-# https://github.com/ConSol/sakuli-examples/archive/feature/sakuli-se.zip
-wget $EXAMPLE_URL/archive/feature/sakuli-se.zip
+wget $EXAMPLE_URL/archive/master.zip
 unzip *.zip
 mkdir -p "$SAKULI_TEST_SUITE"
 for javasuite in "java-selenium-example" "java-example";  do
     echo "Download Sakuli dependencies for v$SAKULI_VERSION"
-    cd $tmpdir/sakuli-examples-feature-sakuli-se/$javasuite
+    cd $tmpdir/sakuli-examples-master/$javasuite
     ls -la
     ## clean target after build, so only dependencies are here
     mvn test clean -Duser.home=$HOME -Dtest=TriggerDownload -DfailIfNoTests=false -Dsakuli.version=$SAKULI_VERSION
 done
 rm -rf "$SAKULI_TEST_SUITE"
-mv $tmpdir/sakuli-examples-feature-sakuli-se/java-example "$SAKULI_TEST_SUITE"
+mv $tmpdir/sakuli-examples-master/java-example "$SAKULI_TEST_SUITE"
 ls -la "$SAKULI_TEST_SUITE"
 rm -rvf $tmpdir
 
