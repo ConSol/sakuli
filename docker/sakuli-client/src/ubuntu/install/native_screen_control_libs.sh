5f97ff8a12ca33ad84cee88a32db0f267ed94b2f
Tobias Schneck <tobias.schneck@consol.de>
2017-10-12 19:56:45 +0200

    fix wrong lsb-release package name

--- docker/sakuli-client/src/ubuntu/install/native_screen_control_libs.sh
+++ docker/sakuli-client/src/ubuntu/install/native_screen_control_libs.sh
@@ -4,5 +4,5 @@ set -e
 
 echo "Install native screen control libraries for Sakuli"
 apt-get update 
-apt-get install -y --fix-missing libcv2.4 libtesseract3 wmctrl xdotool psmisc lsb-releasegit
+apt-get install -y --fix-missing libcv2.4 libtesseract3 wmctrl xdotool psmisc lsb-release
 apt-get clean -y
