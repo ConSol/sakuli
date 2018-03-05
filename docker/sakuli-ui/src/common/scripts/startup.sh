1f6b92fdd5f7ed53fdca36b9d83eee0f1da5b36f
Tobias Schneck <tobias.schneck@consol.de>
2018-03-05 18:37:31 +0100

    add update env `SAKULI_UI_DOCKER_USER_ID` to startup script #308, https://github.com/ConSol/sakuli-ui/issues/14

--- docker/sakuli-ui/src/common/scripts/startup.sh
+++ docker/sakuli-ui/src/common/scripts/startup.sh
@@ -2,6 +2,7 @@
 # have to be added to hold all env vars correctly
 
 main() {
+    export SAKULI_UI_DOCKER_USER_ID=$(id -u)
 	# If arg 1 is not for sakuli ui jar, execute as it is.
 	if [[ "$1" == /* ]] || [[ "$1" == -* ]]; then
         $STARTUPDIR/sakuli_startup.sh "$@"
