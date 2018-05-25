4da2e102d4a3b439aefa7ca2b45439851461be9b
Tobias Schneck <tobias.schneck@consol.de>
2018-05-25 14:45:33 +0200

    fix determination of sakuli version for labs-build CI job

--- .build/set-build-env-vars.sh
+++ .build/set-build-env-vars.sh
@@ -9,7 +9,12 @@ echo "branch= $GIT_BRANCH"
 SAKULI_BRANCH=${GIT_BRANCH/origin\/}
 echo "SAKULI_BRANCH=$SAKULI_BRANCH" > $WORKSPACE/myjob.properties
 
-SAKULI_VERSION="$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')"
+SAKULI_VERSION="$(mvn -q \
+    -Dexec.executable="echo" \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)"
+
 echo "SAKULI_VERSION=$SAKULI_VERSION" >> $WORKSPACE/myjob.properties
 #no difference since https://github.com/ConSol/sakuli/issues/317
 echo "SAKULI_FEATURE_VERSION=$SAKULI_VERSION" >> $WORKSPACE/myjob.properties
